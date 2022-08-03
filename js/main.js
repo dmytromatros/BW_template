865975eddea544d5388bc05f3fd02375f164e4a4
dmytromatros <dimamatroswork@gmail.com>
2022-08-03 19:19:14 +0300

    team-block done

--- js/main.js
+++ js/main.js
@@ -3,7 +3,12 @@ burgerMenu();
 function burgerMenu() {
   let burgerBtn = document.querySelector(".header__burger-icon");
   let burgerUl = document.querySelector(".header__burger-ul");
+  let closeBtn = document.querySelector(".header__burger-close");
   burgerBtn.addEventListener("click", () => {
+    burgerUl.classList.toggle("active-burger");
+    document.style.overflow = "hidden";
+  });
+  closeBtn.addEventListener("click", () => {
     burgerUl.classList.toggle("active-burger");
     document.style.overflow = "hidden";
   });
