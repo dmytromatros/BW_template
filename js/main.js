3c345a1e44f9123b4bb556a8e6128ad139b62cb1
dmytromatros <dimamatroswork@gmail.com>
2022-08-03 13:15:39 +0300

    header done, main-block done

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 "use strict";
+burgerMenu();
+function burgerMenu() {
+  let burgerBtn = document.querySelector(".header__burger-icon");
+  let burgerUl = document.querySelector(".header__burger-ul");
+  burgerBtn.addEventListener("click", () => {
+    burgerUl.classList.toggle("active-burger");
+    document.style.overflow = "hidden";
+  });
+}
